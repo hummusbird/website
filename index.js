bad1617ad1e2bd8fc66f587397738cae731a4646
hummusbird <hummusbirb@gmail.com>
2022-06-23 13:44:05 +0100

    chore: cleanup

--- index.js
+++ index.js
@@ -83,56 +83,12 @@ function moreButton() {
 	document.body.style.overflow = "auto"
 }
 
-async function serverstatus(id) {
-	let NSres = await fetch("https://northstar.tf/client/servers")
-	let NSparsed = await NSres.json()
-
-	let res = await fetch("https://www.hummusbird.co.uk/getServer/" + id + ".txt")
-	let parsed = await res.json()
-
-	if (parsed && Object.keys(parsed).length === 0 && parsed.constructor === Object) {
-		console.log(id + " is offline")
-	}
-	else {
-		document.getElementById(id + "name").innerHTML = (parsed.Name)
-		document.getElementById(id + "map").innerHTML = (parsed.Map)
-		document.getElementById(id + "players").innerHTML = (parsed.Players + "/" + parsed.MaxPlayers + " Players")
-	}
-
-	if (NSparsed && Object.keys(NSparsed).length === 0 && NSparsed.constructor === Object) {
-		console.log(id + " is offline")
-	}
-	else {
-		var birbservers = 0
-		var connected = 0
-		var slots = 0
-		var allservers = 0
-		var allslots = 0
-		var allconnected = 0
-
-		for (var i = 0; i < NSparsed.length; i++) {
-			allservers++
-			allslots += NSparsed[i]["maxPlayers"]
-			allconnected += NSparsed[i]["playerCount"]
-			if (NSparsed[i]["name"].includes("birb")) {
-				birbservers++
-				slots += NSparsed[i]["maxPlayers"]
-				connected += NSparsed[i]["playerCount"]
-			}
-		}
-		document.getElementById("nsname").innerHTML = (birbservers + " birb servers online")
-		document.getElementById("nsplayers").innerHTML = (connected + "/" + slots)
-		document.getElementById("nsmap").innerHTML = (allconnected + "/" + allslots)
-	}
-}
-
 async function cryptoprices(currency) {
-	let res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=Bitcoin%2CEthereum%2CEthereum-Classic%2CRavencoin%2CDogecoin%2C&vs_currencies=USD%2CGBP")
+	let res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=Bitcoin%2CEthereum%2CEthereum-Classic%2CRavencoin%2CDogecoin%2C%2CSolana&vs_currencies=USD%2CGBP")
 	let parsed = await res.json()
 	if (parsed && Object.keys(parsed).length === 0 && parsed.constructor === Object) {
 		document.getElementById("apierror").style.display = ("initial");
 		document.getElementById("crypto").style.display = ("none");
-		document.getElementById("marquee").style.display = ("none");
 	}
 	else {
 		var sign = ""
@@ -155,23 +111,14 @@ async function cryptoprices(currency) {
 
 		document.getElementById("btcRate").innerHTML = (sign + addCommas(parsed.bitcoin[currency]) + " " + currency.toUpperCase())
 		document.getElementById("ethRate").innerHTML = (sign + addCommas(parsed.ethereum[currency]) + " " + currency.toUpperCase())
+		document.getElementById("etcRate").innerHTML = (sign + addCommas(parsed["ethereum-classic"][currency]) + " " + currency.toUpperCase())
 		document.getElementById("dogeRate").innerHTML = (sign + parsed.dogecoin[currency] + " " + currency.toUpperCase())
 		document.getElementById("rvnRate").innerHTML = (sign + parsed.ravencoin[currency] + " " + currency.toUpperCase())
+		document.getElementById("solRate").innerHTML = (sign + parsed.solana[currency] + " " + currency.toUpperCase())
 	}
 
-}
-
-async function cryptomarquee() {
-	let res = await fetch("https://api.coingecko.com/api/v3/status_updates")
-	let parsed = await res.json()
-	if (parsed && Object.keys(parsed).length === 0 && parsed.constructor === Object) {
-		
-	}
-	else {
-		document.getElementById("marqueetext").innerHTML = (parsed.status_updates[0].description.replace(/"/g, "") + "\t" + parsed.status_updates[1].description.replace(/"/g, "") + "\t" + parsed.status_updates[2].description.replace(/"/g, ""))
-	}
 }
 
 function age(){
-	document.getElementById("age").innerHTML = new Date().getFullYear() -2004;
+	document.getElementById("age").innerHTML = new Date().getFullYear() - 2004;
 }
